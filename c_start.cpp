0559b487886a058a56b3f06d960556fa112ae88d
YukiYoshimatsu <45005281+YukiYoshimatsu@users.noreply.github.com>
2019-10-24 11:06:55 -0400

    Create c_start.cpp

---
+++ c_start.cpp
@@ -0,0 +1,13 @@
+//Name:  Yusef Wray
+//Email:  Yusef.Wray25@myhunter.cuny.edu
+//Date:  November 2019
+//My first program in C++
+
+#include <iostream>        //The built-in library for input/output
+using namespace std;       //The names of standard definitions
+
+int main()                 //C++ programs all have a main() function
+{
+  cout << "Hello, World!"; //Print "Hello, World!" to the terminal
+  return 0;                //Standard way to end function
+}
